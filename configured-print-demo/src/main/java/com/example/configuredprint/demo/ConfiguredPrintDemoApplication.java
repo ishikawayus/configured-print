8349a36766f60e1d1d08727f89d9ddf56917b33e
ishikawayus <78156330+ishikawayus@users.noreply.github.com>
2021-04-02 01:09:14 +0900

    Add

---
+++ configured-print-demo/src/main/java/com/example/configuredprint/demo/ConfiguredPrintDemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.configuredprint.demo;
+
+import com.example.configuredprint.ConfiguredPrintManager;
+
+public class ConfiguredPrintDemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println(ConfiguredPrintManager.hello());
+    }
+}
